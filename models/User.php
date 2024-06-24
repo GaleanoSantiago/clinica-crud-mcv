860ad61d2643ea8d99af6ba68a18b2d502bf312f
GaleanoSantiago <104083617+GaleanoSantiago@users.noreply.github.com>
2024-06-24 16:31:35 -0300

    correccion aes_enrypt()

--- models/User.php
+++ models/User.php
@@ -4,7 +4,7 @@ require_once "./../../config/bd.php";
 function Authenticate($username,$password){
     
     $PDO = getConnection();
-    $stament = $PDO->prepare("SELECT * FROM usuarios where user_name='$username' and password=AES_ENCRYPT('PEPE','$password')");
+    $stament = $PDO->prepare("SELECT * FROM usuarios where user_name='$username' and password= AES_ENCRYPT('$password', 'PEPE')");
     return ($stament->execute()) ? $stament->fetchAll() : false;
 }
 
